1445d1145ad9c762c1ff48185422890bba7ea09b
Esad <eakar2012@fau.edu>
2020-10-08 21:41:19 -0400

    asd

---
+++ pages/index.vue
@@ -0,0 +1,171 @@
+<template>
+  <div>
+    <Nav />
+
+    <div class="w-10/12 mx-auto py-6 px-4">
+      <div class="flex flex-row items-start">
+        <div class="shadow w-3/5 mr-4">
+          <div class="p-4 mx-auto">yo</div>
+        </div>
+
+        <div class="w-2/5">
+          <div class="shadow p-4 mb-4">
+            <div
+              class="bg-white rounded-lg flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+            >
+              <div class="flex flex-row justify-between mb-5">
+                <div class="">
+                  <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+                    Running Jobs
+                  </h2>
+                </div>
+                <div class="">
+                  <button class="leading-relaxed text-gray-600">
+                    Create Script
+                  </button>
+                </div>
+              </div>
+
+              <div class="flex flex-col">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th
+                        class="py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Name
+                      </th>
+                      <th
+                        class="py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Last Updated
+                      </th>
+                      <th class="py-2 bg-gray-50"></th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white divide-y divide-gray-200">
+                    <tr v-for="script in scripts" :key="script.title">
+                      <td class="py-2 whitespace-no-wrap">
+                        <div class="text-sm leading-5 text-gray-900">
+                          {{ script.title }}
+                        </div>
+                      </td>
+                      <td class="py-2 whitespace-no-wrap">
+                        <span
+                          class="py-2 whitespace-no-wrap text-sm leading-5 text-gray-500"
+                        >
+                          {{ script.updatedAt }}
+                        </span>
+                      </td>
+                      <td
+                        class="py-2 whitespace-no-wrap text-right text-sm leading-5 font-medium"
+                      >
+                        <a
+                          href="#"
+                          class="text-indigo-600 hover:text-indigo-900"
+                          >Edit</a
+                        >
+                      </td>
+                    </tr>
+
+                    <!-- More rows... -->
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          <div class="shadow p-4">
+            <div
+              class="bg-white rounded-lg flex flex-col md:ml-auto w-full mt-10 md:mt-0"
+            >
+              <div class="flex flex-row justify-between mb-5">
+                <div class="">
+                  <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+                    Scripts
+                  </h2>
+                </div>
+                <div class="">
+                  <button class="leading-relaxed text-gray-600">
+                    Create Script
+                  </button>
+                </div>
+              </div>
+
+              <div class="flex flex-col">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th
+                        class="py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Name
+                      </th>
+                      <th
+                        class="py-2 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Last Updated
+                      </th>
+                      <th class="py-2 bg-gray-50"></th>
+                    </tr>
+                  </thead>
+                  <tbody class="bg-white divide-y divide-gray-200">
+                    <tr v-for="script in scripts" :key="script.title">
+                      <td class="py-2 whitespace-no-wrap">
+                        <div class="text-sm leading-5 text-gray-900">
+                          {{ script.title }}
+                        </div>
+                      </td>
+                      <td class="py-2 whitespace-no-wrap">
+                        <span
+                          class="py-2 whitespace-no-wrap text-sm leading-5 text-gray-500"
+                        >
+                          {{ script.updatedAt }}
+                        </span>
+                      </td>
+                      <td
+                        class="py-2 whitespace-no-wrap text-right text-sm leading-5 font-medium"
+                      >
+                        <a
+                          href="#"
+                          class="text-indigo-600 hover:text-indigo-900"
+                          >Edit</a
+                        >
+                      </td>
+                    </tr>
+
+                    <!-- More rows... -->
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      scripts: [
+        {
+          title: "BTC, moving averate test",
+          updatedAt: "Jan 1st, 2012",
+        },
+        {
+          title: "Algo test #2",
+          updatedAt: "Mar 1st, 2012",
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style>
+</style>
---
+++ plugins/click-outside.vue
@@ -0,0 +1,40 @@
+<script lang="js">
+import Vue from "vue";
+
+let handleOutsideClick;
+
+export default Vue.directive("click-outside", {
+  bind(
+    el,
+    binding,
+    vnode
+  ) {
+    handleOutsideClick = (e) => {
+      e.stopPropagation();
+
+      const { handler, exclude } = binding.value;
+
+      let clickedOnExcludedEl = false;
+      exclude.forEach((refName) => {
+        if (!clickedOnExcludedEl) {
+          const excludedEl = vnode.context.$refs[refName];
+          console.log(excludedEl);
+
+          clickedOnExcludedEl = excludedEl.contains(e.target);
+        }
+      });
+
+      if (!el.contains(e.target) && !clickedOnExcludedEl) {
+        vnode.context[handler]();
+      }
+    };
+    document.addEventListener("click", handleOutsideClick);
+    document.addEventListener("touchstart", handleOutsideClick);
+  },
+
+  unbind() {
+    document.removeEventListener("click", handleOutsideClick);
+    document.removeEventListener("touchstart", handleOutsideClick);
+  },
+});
+</script>
